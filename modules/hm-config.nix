d2a3ccac502798ed57286dea9efc5ed10b97a06b
Ebrahim <53321702+ebrahim37@users.noreply.github.com>
2026-08-14 19:29:24 -0400

    fix foot title

--- modules/hm-config.nix
+++ modules/hm-config.nix
@@ -43,7 +43,7 @@ in
 				PS1='\[\e[31m\][\[\e[33m\]\u\[\e[32m\]@\[\e[34m\]\h \[\e[35m\]\W\[\e[31m\]]\[\e[m\]\$ '
 
 				__update_foot_title() {
-					local title="''${PWD/#$HOME/~}"
+					local title="''${PWD/#"$HOME"/\~}"
 					printf '\e]2;%s\a' "$title"
 				}
 				PROMPT_COMMAND=(__update_foot_title "''${PROMPT_COMMAND[@]}")
